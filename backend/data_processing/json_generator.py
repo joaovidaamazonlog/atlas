53db4c72b37f0dfe93b62bcc89ce0ac76794e1f1
João Victor Teodoro da Silva <joao.amazonlog@gmail.com>
2025-12-12 14:49:05 -0300

    Update

--- backend/data_processing/json_generator.py
+++ backend/data_processing/json_generator.py
@@ -64,10 +64,10 @@ class JsonGenerator:
         link_whatsapp = ''
         if pd.notna(row.get("Phone")):
             phone_number = row.get("Phone").translate(str.maketrans({"(": "", ")": "", " ": "", "-": "", "+": ""}))
-            link_whatsapp = f'Enviar menssagem <a href="https://wa.me/{phone_number}" target="_blank"><i class="fa fa-whatsapp"></i></a>'
+            link_whatsapp = f'Enviar menssagem <a href="https://wa.me/{phone_number}" target="_blank"><i class="fa fa-whatsapp" style="font-size:24px"></i></a>'
             
 
-        return f'<div style="width: auto; max-height: 400px; font-size: 12px;">{info_html}<hr class: "my-2">{link_salesforce}<br>{link_whatsapp}><hr class: "my-2">{metrics_html}</div>'
+        return f'<div style="width: auto; max-height: 400px; font-size: 12px;">{info_html}<hr class: "my-2">{link_salesforce}<br>{link_whatsapp}<hr class: "my-2">{metrics_html}</div>'
 
     @staticmethod
     def generate_json(period: dict, final_df: pd.DataFrame, output_path: str):

5e2cede2978a9bf1d32107b4f38c8365ca988c7b
João Victor Teodoro da Silva <joao.amazonlog@gmail.com>
2026-02-10 11:35:29 -0300

    Update

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ const DataManager = {
             fetch('https://joaovidaamazonlog.github.io/atlas/data/jurisdiction.geojson').then(res => res.json()),
             fetch('https://joaovidaamazonlog.github.io/atlas/data/optimization_data.geojson').then(res => res.json()).catch(() => null)
         ]).then(([partnerData, polygonData, jurisdictionData, optData]) => {
-            AppState.allMarkersData = partnerData.allMarkersData;
+            AppState.allMarkersData = partnerData.allMarkerData;
             AppState.period = partnerData.period;
             AppState.deliveryStations = partnerData.deliveryStations;
             AppState.polygonsData = polygonData;
